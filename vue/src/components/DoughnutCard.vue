84a66217350cc10f425b78aca5533247b818b49d
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-04-30 18:53:14 -0400

    asdf

--- vue/src/components/DoughnutCard.vue
+++ vue/src/components/DoughnutCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <RouterLink to="/doughnut" id="link">Trees in NY</RouterLink>
-  </template>
-  
-  <script>
-  import { RouterLink } from 'vue-router';
-  export default {
-      name: "DoughnutCard",
-      props: {},
-      components: { RouterLink }
-  }
-  </script>
+  <RouterLink to="/doughnut" id="link">Trees of NYC</RouterLink>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+export default {
+  name: 'DoughnutCard',
+  props: {},
+  components: { RouterLink }
+}
+</script>
