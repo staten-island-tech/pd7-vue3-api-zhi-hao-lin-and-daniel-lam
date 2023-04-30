8ba113e162d210d40489c97b21195c97c5bcdd9c
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-04-30 11:35:28 -0400

    asdf

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const squirrel = ref('')
+async function getSquirrel() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  let data = await res.json()
+  squirrel.values = data.results
+}
+onMounted(() => {
+  getSquirrel()
+})
+</script>
+
+<style scoped></style>
