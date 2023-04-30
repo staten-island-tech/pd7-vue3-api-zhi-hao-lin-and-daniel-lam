71b7b08e8dda549e26e0628ea1c80aff25826c9c
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-04-30 11:51:34 -0400

    ads

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -3,14 +3,14 @@
 </template>
 
 <script setup>
-import { ref, Created } from 'vue'
+import { ref, onMounted } from 'vue'
 const squirrel = ref('')
 async function getSquirrel() {
   let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
   let data = await res.json()
   squirrel.value = data.results
 }
-Created(() => {
+onMounted(() => {
   getSquirrel()
 })
 </script>
