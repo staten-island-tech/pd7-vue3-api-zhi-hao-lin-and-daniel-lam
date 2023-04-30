c2a81db478b8ce27774f41350587c79503e12ca4
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-04-30 11:38:19 -0400

    dsa

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -8,7 +8,7 @@ const squirrel = ref('')
 async function getSquirrel() {
   let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
   let data = await res.json()
-  squirrel.values = data.results
+  squirrel.value = data.results
 }
 onMounted(() => {
   getSquirrel()
