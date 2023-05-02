e381dc2d35021afae8fe48701fa23a0d7878ef76
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-05-01 20:28:32 -0400

    adsf

--- vue/src/components/DoughnutChart.vue
+++ vue/src/components/DoughnutChart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chart">
     <RouterLink to="/">Back to Main</RouterLink>
-    <h1>Trees in Each Borough</h1>
+    <h1>Types of Trees</h1>
     <Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
   </div>
 </template>
@@ -19,13 +19,35 @@ export default {
     return {
       loaded: false,
       chartData: {
-        labels: ['Brooklyn', 'Queens', 'Manhattan', 'Bronx', 'Staten Island'],
+        labels: [
+          'London planetree',
+          'honeylocust',
+          'American linden',
+          'red maple',
+          'ginkgo',
+          'willow oak',
+          'sycamore maple',
+          'Amur maple',
+          'pin oak',
+          'hedge maple'
+        ],
         datasets: [{ data: [] }]
       },
       chartOptions: {
         responsive: true,
         maintainAspectRatio: true,
-        backgroundColor: ['#963a2f', '#d58258', '#ecb984', '#fffee9', '#a8a676']
+        backgroundColor: [
+          '#963a2f',
+          '#d58258',
+          '#ecb984',
+          '#fffee9',
+          '#a8a676',
+          '#c37367',
+          '#e8d3a3',
+          '#ffa633',
+          '#5d6f66',
+          '#e77e9b'
+        ]
       }
     }
   },
@@ -33,16 +55,26 @@ export default {
     try {
       const res = await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json')
       const treeData = await res.json()
-      const red = treeData.filter((tree) => tree.boroname === 'Brooklyn')
+      const red = treeData.filter((tree) => tree.spc_common === 'London planetree')
       this.chartData.datasets[0].data.push(red.length)
-      const yellow = treeData.filter((tree) => tree.boroname === 'Queens')
+      const yellow = treeData.filter((tree) => tree.spc_common === 'honeylocust')
       this.chartData.datasets[0].data.push(yellow.length)
-      const blue = treeData.filter((tree) => tree.boroname === 'Manhattan')
+      const blue = treeData.filter((tree) => tree.spc_common === 'American linden')
       this.chartData.datasets[0].data.push(blue.length)
-      const green = treeData.filter((tree) => tree.boroname === 'Bronx')
+      const green = treeData.filter((tree) => tree.spc_common === 'red maple')
       this.chartData.datasets[0].data.push(green.length)
-      const purple = treeData.filter((tree) => tree.boroname === 'Staten Island')
+      const purple = treeData.filter((tree) => tree.spc_common === 'ginkgo')
       this.chartData.datasets[0].data.push(purple.length)
+      const orange = treeData.filter((tree) => tree.spc_common === 'willow oak')
+      this.chartData.datasets[0].data.push(orange.length)
+      const pink = treeData.filter((tree) => tree.spc_common === 'sycamore maple')
+      this.chartData.datasets[0].data.push(pink.length)
+      const brown = treeData.filter((tree) => tree.spc_common === 'Amur maple')
+      this.chartData.datasets[0].data.push(brown.length)
+      const black = treeData.filter((tree) => tree.spc_common === 'pin oak')
+      this.chartData.datasets[0].data.push(black.length)
+      const grey = treeData.filter((tree) => tree.spc_common === 'hedge maple')
+      this.chartData.datasets[0].data.push(grey.length)
       this.loaded = true
     } catch (e) {
       console.error(e)
@@ -55,7 +87,7 @@ export default {
 h1 {
   text-align: center;
   color: rgb(73, 66, 53);
-  font-size: 5rem;
+  font-size: 2.75rem;
 }
 h1:hover {
   animation: shake 0.5s;
---
+++ vue/src/views/DartView.vue
@@ -0,0 +1,11 @@
+<template>
+  <DoughnutChart />
+</template>
+
+<script>
+import DartChart from '../components/DartChart.vue'
+export default {
+  name: 'App',
+  components: { DartChart }
+}
+</script>
